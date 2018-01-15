72300ff9ca17f61116c4e93e0f47303bbd045564
Wilber <wmartin8@gmail.com>
2018-01-14 21:35:02 -0500

    create project. call wfc service.

---
+++ AdDataAggregator/Controllers/AdsController.cs
@@ -0,0 +1,21 @@
+﻿using AdDataAggregator.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AdDataAggregator.Controllers
+{
+    public class AdsController : ApiController
+    {
+        public IEnumerable<Ad> GetAllAds() {
+            var client = new AdDataServiceClient();
+            var ads = client.GetAdDataByDateRange(new DateTime(2011, 1, 1), new DateTime(2011, 4, 1));
+            client.Close();
+
+            return ads;
+        }
+    }
+}
